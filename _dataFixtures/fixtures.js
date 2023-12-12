c2a9b56eb8c1de4e422c76aeb7a8cb49018db0c8
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-12-12 17:27:59 +0100

    fixtures test

--- _dataFixtures/fixtures.js
+++ _dataFixtures/fixtures.js
@@ -1,1 +1,32 @@
+// /*============ IMPORT USED MODULES ============*/
+const { faker } = require('@faker-js/faker');
+
+const User = require('./../_models/IUser');
+
+
+// /*============ GENERATE FIXTURES ============*/
+const generateFakeUserData = () => {
+    return {
+        email: faker.internet.email(),
+        pseudo: faker.internet.userName(),
+        password: faker.internet.password(),
+        registeredAt: faker.date.past()
+    };
+};
+
+const createFakeUser = async () => {
+    const fakeUserData = generateFakeUserData();
+
+    try {
+        const newUser = new User(fakeUserData);
+        await newUser.save();
+        console.log('Utilisateur ajouté à la base de données:', newUser);
+    }
+    catch (error) {
+        console.error('Erreur lors de l\'ajout de l\'utilisateur à la base de données:', error);
+    }
+};
+
+// Générer un utilisateur fictif et l'ajouter à la base de données
+createFakeUser();
 
