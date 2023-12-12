e065e74d85ed615f0e5395b6b5be1b7f4111fb47
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-12-12 18:54:44 +0100

    add swagger doc

---
+++ swagger.js
@@ -0,0 +1,24 @@
+/*============ IMPORT USED MODULES ============*/
+const swaggerJSDoc = require('swagger-jsdoc');
+
+
+/*============ SWAGGER MODULE ============*/
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+        title: 'API',
+        version: '1.0.0',
+        description: 'Application Blog',
+        },
+    },
+    apis: ['./routers/authRouter.js',
+        './routers/usersRouter.js',
+        './routers/articlesRouter.js']
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+
+/*============ EXPORT MODULE ============*/
+module.exports = swaggerSpec;
