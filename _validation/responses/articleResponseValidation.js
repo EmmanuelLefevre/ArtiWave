8eec2d7fdabbd4f8a323a2dc147ac72b01915735
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-12-15 23:00:20 +0100

    maj response schema

--- _validation/responses/articleResponseValidation.js
+++ _validation/responses/articleResponseValidation.js
@@ -4,8 +4,10 @@ const { object, string, array, number } = require('yup');
 
 /*============ VALIDATION SCHEMA ============*/
 
-/*=== ARTICLE RESPONSE VALIDATION ===*/
-const articleResponseValidation = object({
+/*=== GET ARTICLE ===*/
+
+/*----- ADMIN ARTICLE RESPONSE VALIDATION -----*/
+const articleResponseValidationRoleAdmin = object({
     _id: string().matches(/^[0-9a-fA-F]{24}$/).required(),
     title: string().required(),
     content: string().required(),
@@ -17,16 +19,54 @@ const articleResponseValidation = object({
     updatedAt: string().required()
 });
 
+/*----- BASE ARTICLE RESPONSE VALIDATION -----*/
+const articleResponseValidationBase = object({
+    title: string().required(),
+    content: string().required(),
+    author: object({
+        nickname: string().required(),
+    }).required(),
+    createdAt: string().required(),
+    updatedAt: string().required()
+});
 
-/*=== GET ALL ARTICLE RESPONSE VALIDATION ===*/
-const articlesResponseValidation = object({
-    data: array().of(articleResponseValidation).required(),
+
+/*=== GET ALL ARTICLES ===*/
+
+/*----- ADMIN GET ALL ARTICLE RESPONSE VALIDATION -----*/
+const articlesResponseValidationRoleAdmin = object({
+    data: array().of(articleResponseValidationRoleAdmin).required(),
     dataCount: number().integer().required()
+});
+
+/*----- BASE GET ALL ARTICLE RESPONSE VALIDATION -----*/
+const articlesResponseValidationBase = object({
+    data: array().of(articleResponseValidationBase).required(),
+    dataCount: number().integer().required()
+});
+
+
+/*=== UPDATE ARTICLE ===*/
+
+/*----- ADMIN ARTICLE UPDATED RESPONSE VALIDATION -----*/
+const articleUpdatedResponseValidationRoleAdmin = object({
+    data: array().of(articleResponseValidationRoleAdmin).required(),
+    modifiedProperties: object().required()
+});
+
+/*----- BASE ARTICLE UPDATED RESPONSE VALIDATION -----*/
+const articleUpdatedResponseValidationBase = object({
+    data: array().of(articleResponseValidationBase).required(),
+    modifiedProperties: object().required()
 });
 
 
 /*============ EXPORT MODULE ============*/
 module.exports = {
-    articleResponseValidation,
-    articlesResponseValidation
+    articleResponseValidationRoleAdmin,
+    articleResponseValidationBase,
+    articlesResponseValidationRoleAdmin,
+    articlesResponseValidationBase,
+    articleUpdatedResponseValidationRoleAdmin,
+    articleUpdatedResponseValidationBase
 };
