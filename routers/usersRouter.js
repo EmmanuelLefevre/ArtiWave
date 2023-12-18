f5a08b7975a94e33fcb4f49cff2b3ffe4f106a7c
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-12-18 12:18:19 +0100

    move ACL in router

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -8,6 +8,7 @@ const { registerLimiter } = require('../middleware/rateLimiter');
 const jwtCheck = require('../middleware/jwtCheck');
 const accountCheck = require('../middleware/accountCheck');
 const premiumCheck = require('../middleware/premiumCheck');
+const allowedCurrentMethodCheck = require('../middleware/allowedCurrentMethodCheck');
 
 const ErrorHandler = require('../_errors/errorHandler');
 const ValidationErrorHandler = require('../_validation/validationErrorHandler');
@@ -28,161 +29,169 @@ router.use(usersLogs);
 /*============ ROUTES FOR USERS ============*/
 
 /*=== REGISTER ===*/
-router.post('/register', [
-    userValidationRule,
-    (req, res, next) => {
+router.route('/register')
+    .all(allowedCurrentMethodCheck(['POST']))
+    .post([
+        userValidationRule,
+        (req, res, next) => {
+            try {
+                // Check presence of data email && password && nickname
+                const { email, password, nickname } = req.body;
 
-        try {
-            // Check presence of data email && password && nickname
-            const { email, password, nickname } = req.body;
+                if (!email || !password || !nickname) {
+                    return res.status(400).json({ message: 'Invalid request!' });
+                }
 
-            if (!email || !password || !nickname) {
-                return res.status(400).json({ message: 'Invalid request!' });
+                // Pass to validation middleware
+                next();
             }
-
-            // Pass to validation middleware
-            next();
-        }
-        catch (err) {
-            return ErrorHandler.sendInternalServerError(res, err);
-        }
-    },
-    async (req, res, next) => {
-        try {
-            const errors = validationResult(req);
-
-            if (!errors.isEmpty()) {
-                return ValidationErrorHandler.handle(res, errors);
+            catch (err) {
+                return ErrorHandler.sendInternalServerError(res, err);
             }
+        },
+        async (req, res, next) => {
+            try {
+                const errors = validationResult(req);
 
-            // Pass to registerLimiter middleware
-            next();
+                if (!errors.isEmpty()) {
+                    return ValidationErrorHandler.handle(res, errors);
+                }
+
+                // Pass to registerLimiter middleware
+                next();
+            }
+            catch (err) {
+                return ErrorHandler.sendInternalServerError(res, err);
+            }
+        },
+        registerLimiter,
+        async (req, res) => {
+            try {
+                // Validation successful, proceed with registration
+                await usersController.register(req, res);
+            }
+            catch (err) {
+                return ErrorHandler.sendInternalServerError(res, err);
+            }
         }
-        catch (err) {
-            return ErrorHandler.sendInternalServerError(res, err);
-        }
-    },
-    registerLimiter,
-    async (req, res) => {
-        try {
-            // Validation successful, proceed with registration
-            await usersController.register(req, res);
-        }
-        catch (err) {
-            return ErrorHandler.sendInternalServerError(res, err);
-        }
-    }
-]);
+    ]);
 
 
 /*=== GET ALL USERS ===*/
-router.get('/',
-    jwtCheck,
-    premiumCheck,
-    async (req, res) => {
-
-    try {
-        await usersController.getAllUsers(req, res);
-    }
-    catch (err) {
-        return ErrorHandler.sendInternalServerError(res, err);
-    }
-});
+router.route('/')
+    .all(allowedCurrentMethodCheck(['GET']))
+    .get([
+        jwtCheck,
+        premiumCheck,
+        async (req, res) => {
+            try {
+                await usersController.getAllUsers(req, res);
+            }
+            catch (err) {
+                return ErrorHandler.sendInternalServerError(res, err);
+            }
+        }
+    ]);
 
 
 /*=== GET SINGLE USER ===*/
-router.get('/:id',
-    accountCheck,
-    jwtCheck,
-    validateURIParam('id'),
-    async (req, res) => {
+router.route('/:id')
+    .all(allowedCurrentMethodCheck(['GET']))
+    .get([
+        accountCheck,
+        jwtCheck,
+        validateURIParam('id'),
+        async (req, res) => {
+            try {
+                const errors = validationResult(req);
 
-    try {
-        const errors = validationResult(req);
+                if (!errors.isEmpty()) {
+                    return ValidationErrorHandler.handle(res, errors);
+                }
 
-        if (!errors.isEmpty()) {
-            return ValidationErrorHandler.handle(res, errors);
+                await usersController.getUser(req, res);
+            }
+            catch (err) {
+                return ErrorHandler.sendInternalServerError(res, err);
+            }
         }
-
-        await usersController.getUser(req, res);
-    }
-    catch (err) {
-        return ErrorHandler.sendInternalServerError(res, err);
-    }
-});
+    ]);
 
 
 /*=== UPDATE USER ===*/
-router.patch('/:id', [
-    jwtCheck,
-    validateURIParam('id'),
-    // Custom validation rules
-    userValidationRule,
-    (req, res, next) => {
+router.route('/:id')
+    .all(allowedCurrentMethodCheck(['PATCH']))
+    .patch([
+        jwtCheck,
+        validateURIParam('id'),
+        // Custom validation rules
+        userValidationRule,
+        (req, res, next) => {
+            try {
+                // Check presence of forbidden paramaters roles || registeredAt || updatedAt
+                const params = ['roles', 'registeredAt', 'updatedAt'];
+                const forbiddenParams = Object.keys(req.body).filter(param => params.includes(param));
 
-        try {
-            // Check presence of forbidden paramaters roles || registeredAt || updatedAt
-            const params = ['roles', 'registeredAt', 'updatedAt'];
-            const forbiddenParams = Object.keys(req.body).filter(param => params.includes(param));
-
-            if (forbiddenParams.length > 0) {
-                const errorMessage = `These parameters can't be modified: ${forbiddenParams.join(' and ')}`;
-                return res.status(400).json({ message: errorMessage });
-            }
-
-            // Check presence of at least parameter email || nickname || password
-            if (req.method === 'PATCH') {
-                const allowedProperties = ['email', 'nickname', 'password'];
-                const isValidUpdate = Object.keys(req.body).some(prop => allowedProperties.includes(prop));
-
-                if (!isValidUpdate) {
-                    return res.status(400).json({ message: 'Invalid request!' });
+                if (forbiddenParams.length > 0) {
+                    const errorMessage = `These parameters can't be modified: ${forbiddenParams.join(' and ')}`;
+                    return res.status(400).json({ message: errorMessage });
                 }
+
+                // Check presence of at least parameter email || nickname || password
+                if (req.method === 'PATCH') {
+                    const allowedProperties = ['email', 'nickname', 'password'];
+                    const isValidUpdate = Object.keys(req.body).some(prop => allowedProperties.includes(prop));
+
+                    if (!isValidUpdate) {
+                        return res.status(400).json({ message: 'Invalid request!' });
+                    }
+                }
+
+                // Pass to validation middleware
+                next();
             }
+            catch (err) {
+                return ErrorHandler.sendInternalServerError(res, err);
+            }
+        },
+        async (req, res) => {
+            try {
+                const errors = validationResult(req);
 
-            // Pass to validation middleware
-            next();
+                if (!errors.isEmpty()) {
+                    return ValidationErrorHandler.handle(res, errors);
+                }
+
+                await usersController.updateUser(req, res);
+            }
+            catch (err) {
+                return ErrorHandler.sendInternalServerError(res, err);
+            }
         }
-        catch (err) {
-            return ErrorHandler.sendInternalServerError(res, err);
-        }
-    }
-], async (req, res) => {
-
-    try {
-        const errors = validationResult(req);
-
-        if (!errors.isEmpty()) {
-            return ValidationErrorHandler.handle(res, errors);
-        }
-
-        await usersController.updateUser(req, res);
-    }
-    catch (err) {
-        return ErrorHandler.sendInternalServerError(res, err);
-    }
-});
+    ]);
 
 
 /*=== DELETE USER ===*/
-router.delete('/:id',
-    jwtCheck,
-    validateURIParam('id'),
-    async (req, res) => {
+router.route('/:id')
+    .all(allowedCurrentMethodCheck(['DELETE']))
+    .delete([
+        jwtCheck,
+        validateURIParam('id'),
+        async (req, res) => {
+            try {
+                const errors = validationResult(req);
 
-    try {
-        const errors = validationResult(req);
+                if (!errors.isEmpty()) {
+                    return ValidationErrorHandler.handle(res, errors);
+                }
 
-        if (!errors.isEmpty()) {
-            return ValidationErrorHandler.handle(res, errors);
+                await usersController.deleteUser(req, res);
+            }
+            catch (err) {
+                return ErrorHandler.sendInternalServerError(res, err);
+            }
         }
-
-        await usersController.deleteUser(req, res);
-    }
-    catch (err) {
-        return ErrorHandler.sendInternalServerError(res, err);
-    }
-});
+    ]);
 
 
 /*============ EXPORT MODULE ============*/
