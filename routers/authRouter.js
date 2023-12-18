c77327a64bf5ec347bd08a3657c72a3a46a3b8a9
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-12-18 12:06:44 +0100

    move ACL in router

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { validationResult } = require('express-validator');
 
 const authController = require('../controllers/authController');
+const allowedCurrentMethodCheck = require('../middleware/allowedCurrentMethodCheck');
 
 const ErrorHandler = require('../_errors/errorHandler');
 const ValidationErrorHandler = require('../_validation/validationErrorHandler');
@@ -22,39 +23,41 @@ router.use(authLogs);
 /*============ ROUTES FOR AUTHENTIFICATION ============*/
 
 /*=== LOGIN ===*/
-router.post('/', [
-    userValidationRule,
-    (req, res, next) => {
+router.route('/')
+    .all(allowedCurrentMethodCheck(['POST']))
+    .post([
+        userValidationRule,
+        (req, res, next) => {
+            try {
+                // Check presence of data email && password
+                const { email, password } = req.body;
 
-        try {
-            // Check presence of data email && password
-            const { email, password } = req.body;
+                if (!email || !password ) {
+                    return res.status(400).json({ message: 'Invalid request!' });
+                }
 
-            if (!email || !password ) {
-                return res.status(400).json({ message: 'Invalid request!' });
+                // Pass to validation middleware
+                next();
             }
+            catch (err) {
+                return ErrorHandler.sendInternalServerError(res, err);
+            }
+        },
+        async (req, res) => {
+            try {
+                const errors = validationResult(req);
 
-            // Pass to validation middleware
-            next();
-        }
-        catch (err) {
-            return ErrorHandler.sendInternalServerError(res, err);
-        }
-    }
-], async (req, res) => {
-    try {
-        const errors = validationResult(req);
+                if (!errors.isEmpty()) {
+                    return ValidationErrorHandler.handle(res, errors);
+                }
 
-        if (!errors.isEmpty()) {
-            return ValidationErrorHandler.handle(res, errors);
+                await authController.login(req, res);
+            }
+            catch (err) {
+                return ErrorHandler.sendInternalServerError(res, err);
+            }
         }
-
-        await authController.login(req, res);
-    }
-    catch (err) {
-        return ErrorHandler.sendInternalServerError(res, err);
-    }
-});
+    ]);
 
 
 /*============ EXPORT MODULE ============*/
