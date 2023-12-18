3273101867f331809dabf78e0ada0fa68d105995
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-12-18 12:09:38 +0100

    move ACL in router

--- routers/adminsRouter.js
+++ routers/adminsRouter.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { validationResult } = require('express-validator');
 
 const adminsController = require('../controllers/adminsController');
+const allowedCurrentMethodCheck = require('../middleware/allowedCurrentMethodCheck');
 
 const ErrorHandler = require('../_errors/errorHandler');
 const ValidationErrorHandler = require('../_validation/validationErrorHandler');
@@ -22,69 +23,73 @@ router.use(adminsLogs);
 /*============ ROUTES FOR ADMINS============*/
 
 /*=== DELETE ALL USERS ===*/
-router.delete('/delete_all_users',
-    async (req, res) => {
-
-    try {
-        await adminsController.deleteAllUsers(req, res);
-    }
-    catch (err) {
-        return ErrorHandler.sendInternalServerError(res, err);
-    }
-});
+router.route('/delete_all_users')
+    .all(allowedCurrentMethodCheck(['DELETE']))
+    .delete(async (req, res) => {
+        try {
+            await adminsController.deleteAllUsers(req, res);
+        }
+        catch (err) {
+            return ErrorHandler.sendInternalServerError(res, err);
+        }
+    });
 
 
 /*=== DELETE ALL ARTICLES ===*/
-router.delete('/delete_all_articles',
-    async (req, res) => {
-
-    try {
-        await adminsController.deleteAllArticles(req, res);
-    }
-    catch (err) {
-        return ErrorHandler.sendInternalServerError(res, err);
-    }
-});
+router.route('/delete_all_articles')
+    .all(allowedCurrentMethodCheck(['DELETE']))
+    .delete(async (req, res) => {
+        try {
+            await adminsController.deleteAllArticles(req, res);
+        }
+        catch (err) {
+            return ErrorHandler.sendInternalServerError(res, err);
+        }
+    });
 
 
 /*=== DELETE ALL ARTICLES BY USER ===*/
-router.delete('/delete_all_articles/:id',
-    validateURIParam('id'),
-    async (req, res) => {
+router.route('/delete_all_articles/:id')
+    .all(allowedCurrentMethodCheck(['DELETE']))
+    .delete(
+        validateURIParam('id'),
+        async (req, res) => {
+            try {
+                const errors = validationResult(req);
 
-    try {
-        const errors = validationResult(req);
+                if (!errors.isEmpty()) {
+                    return ValidationErrorHandler.handle(res, errors);
+                }
 
-        if (!errors.isEmpty()) {
-            return ValidationErrorHandler.handle(res, errors);
+                await adminsController.deleteAllArticlesByUser(req, res);
+            }
+            catch (err) {
+                return ErrorHandler.sendInternalServerError(res, err);
+            }
         }
-
-        await adminsController.deleteAllArticlesByUser(req, res);
-    }
-    catch (err) {
-        return ErrorHandler.sendInternalServerError(res, err);
-    }
-});
+    );
 
 
 /*=== INVERT USER ROLE ===*/
-router.patch('/invert_user_role/:id',
-    validateURIParam('id'),
-    async (req, res) => {
+router.route('/invert_user_role/:id')
+    .all(allowedCurrentMethodCheck(['PATCH']))
+    .patch([
+        validateURIParam('id'),
+        async (req, res) => {
+            try {
+                const errors = validationResult(req);
 
-    try {
-        const errors = validationResult(req);
+                if (!errors.isEmpty()) {
+                    return ValidationErrorHandler.handle(res, errors);
+                }
 
-        if (!errors.isEmpty()) {
-            return ValidationErrorHandler.handle(res, errors);
+                await adminsController.invertUserRole(req, res);
+            }
+            catch (err) {
+                return ErrorHandler.sendInternalServerError(res, err);
+            }
         }
-
-        await adminsController.invertUserRole(req, res);
-    }
-    catch (err) {
-        return ErrorHandler.sendInternalServerError(res, err);
-    }
-});
+    ]);
 
 
 /*============ EXPORT MODULE ============*/
