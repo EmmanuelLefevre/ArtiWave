d5033ec6bddca6cda9ac529b9e09ce791ddb5542
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-12-19 14:37:46 +0100

    make class authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,53 +19,60 @@ let failedLoginAttempts = 0;
 // Store timestamp of the last failed login attempt
 let lastFailedLoginDate = null;
 
-exports.login = (req, res) => {
-    const { email, password } = req.body;
+class AuthController {
+    static login(req, res) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const { email, password } = req.body;
 
-    return new Promise((resolve, reject) => {
-        try {
-            if (lastFailedLoginDate && failedLoginAttempts >= 5) {
-                const currentTime = new Date();
-                const timeDiff = currentTime - lastFailedLoginDate;
+                if (lastFailedLoginDate && failedLoginAttempts >= 5) {
+                    const currentTime = new Date();
+                    const timeDiff = currentTime - lastFailedLoginDate;
 
-                // Block attempts for one hour
-                if (timeDiff < 60 * 60 * 1000) {
-                    throw new LoginLimiterError();
+                    // Block attempts for one hour
+                    if (timeDiff < 60 * 60 * 1000) {
+                        throw new LoginLimiterError();
+                    }
+                    else {
+                        // Reset counter after one hour
+                        failedLoginAttempts = 0;
+                    }
+                }
+
+                await AuthService.login(email, password)
+                    .then(response => {
+                        // Reset counter when login is successful
+                        failedLoginAttempts = 0;
+
+                        // Send response
+                        resolve(res.status(200).json(response));
+                    })
+                    .catch(err => {
+                        failedLoginAttempts++;
+                        lastFailedLoginDate = new Date();
+
+                        if (err instanceof BadCredentialsError ||
+                            err instanceof ResponseValidationError ||
+                            err instanceof InternalServerError) {
+                            reject(res.status(err.statusCode).json({ message: err.message }));
+                        }
+                        else {
+                            reject(InternalServerError());
+                        }
+                    });
+            }
+            catch (err) {
+                if (err instanceof LoginLimiterError) {
+                    reject(res.status(err.statusCode).json({ message: err.message }));
                 }
                 else {
-                    // Reset counter after one hour
-                    failedLoginAttempts = 0;
+                    reject(InternalServerError());
                 }
             }
+        })
+    }
+}
 
-            AuthService.login(email, password)
-                .then(response => {
-                    // Reset counter when login is successful
-                    failedLoginAttempts = 0;
 
-                    // Send response
-                    resolve(res.status(200).json(response));
-                })
-                .catch(err => {
-                    failedLoginAttempts++;
-                    lastFailedLoginDate = new Date();
-
-                    if (err instanceof BadCredentialsError ||
-                        err instanceof ResponseValidationError ||
-                        err instanceof InternalServerError) {
-                        reject(res.status(err.statusCode).json({ message: err.message }));
-                    } else {
-                        reject(InternalServerError());
-                    }
-                });
-        }
-        catch (err) {
-            if (err instanceof LoginLimiterError) {
-                reject(res.status(err.statusCode).json({ message: err.message }));
-            }
-            else {
-                reject(InternalServerError());
-            }
-        }
-    });
-};
+/*============ EXPORT MODULE ============*/
+module.exports = AuthController;
