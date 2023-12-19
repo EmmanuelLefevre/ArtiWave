792ea9f3fd80f8a2fa8f846d44d83b09ee268359
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-12-19 15:38:07 +0100

    maj

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -5,80 +5,85 @@
 
 /*============ IMPORT USED MODULES ============*/
 const express = require('express');
-const { validationResult } = require('express-validator');
+
+const allowedCurrentMethodCheck = require('../middleware/allowedCurrentMethodCheck');
 
 const authController = require('../controllers/authController');
-const allowedCurrentMethodCheck = require('../middleware/allowedCurrentMethodCheck');
 
 const InternalServerError = require('../_errors/internalServerError');
 const InvalidRequestError = require('../_errors/invalidRequestError');
 const ValidationError = require('../_errors/validationError');
 
+const { validationResult } = require('express-validator');
 const userValidationRule = require('../_validation/validators/userValidator');
 
 const { authLogs } = require('../_logs/auth/authLogger');
 
 
-/*============ EXPRESS ROUTER ============*/
-const AuthRouter = express.Router();
-
-
-/*============ MIDDLEWARE REQUEST LOGS ============*/
-AuthRouter.use(authLogs);
-
-
-/*============ ROUTES FOR AUTHENTIFICATION ============*/
+/*============ ROUTE FOR AUTHENTIFICATION ============*/
 
 /*=== LOGIN ===*/
-AuthRouter.route('/')
-    .all(allowedCurrentMethodCheck(['POST']))
-    .post([
-        userValidationRule,
-        (req, res, next) => {
-            try {
-                // Check presence of data email && password
-                const { email, password } = req.body;
+class AuthRouterHandler {
+    static init() {
+        // Express router
+        const AuthRouter = express.Router();
+        // Middleware request logs
+        AuthRouter.use(authLogs);
 
-                if (!email || !password ) {
-                    throw new InvalidRequestError();
-                }
+        AuthRouter.route('/')
+            .all(allowedCurrentMethodCheck(['POST']))
+            .post([
+                userValidationRule,
+                AuthRouterHandler.validateRequest,
+                AuthRouterHandler.loginHandler
+            ]);
 
-                // Pass to validation middleware
-                next();
+        return AuthRouter;
+    }
+
+    static validateRequest(req, res, next) {
+        try {
+            const { email, password } = req.body;
+
+            if (!email || !password) {
+                throw new InvalidRequestError();
             }
-            catch (err) {
-                if (err instanceof InvalidRequestError) {
-                    return res.status(err.statusCode).json({ message: err.message });
-                }
-                else {
-                    throw InternalServerError();
-                }
-            }
-        },
-        async (req, res) => {
-            try {
-                const errors = validationResult(req);
 
-                if (!errors.isEmpty()) {
-                    throw new ValidationError(errors.array(), 422);
-                }
-
-                await authController.login(req, res);
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                throw new ValidationError(errors.array());
             }
-            catch (err) {
-                if (err instanceof ValidationError) {
-                    return res.status(err.statusCode).json(err.getErrorResponse());
-                }
-                else if (err instanceof InternalServerError) {
-                    return res.status(err.statusCode).json({ message: err.message });
-                }
-                else {
-                    return new InternalServerError();
-                }
+
+            next();
+        }
+        catch (err) {
+            if (err instanceof InvalidRequestError) {
+                return res.status(err.statusCode).json({ message: err.message });
+            }
+            else if (err instanceof ValidationError) {
+                return res.status(err.statusCode).json(err.getErrorResponse());
+            }
+            else {
+                throw new InternalServerError();
             }
         }
-    ]);
+    }
+
+    static async loginHandler(req, res) {
+        try {
+            await authController.login(req, res);
+        }
+        catch (err) {
+            if (err instanceof InternalServerError) {
+                return res.status(err.statusCode).json({ message: err.message });
+            }
+            else {
+                return new InternalServerError();
+            }
+        }
+    }
+}
 
 
 /*============ EXPORT MODULE ============*/
-module.exports = AuthRouter;
+module.exports = AuthRouterHandler.init();
