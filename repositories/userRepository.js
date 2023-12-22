123e3b3d4671a32024dfe0c3abda196e141d7028
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-12-22 03:36:03 +0100

    maj

--- repositories/userRepository.js
+++ repositories/userRepository.js
@@ -15,7 +15,7 @@ const UserNotFoundError = require('../_errors/userNotFoundError');
 /*============ USER REPOSITORY ============*/
 class UserRepository {
 
-    /*=== FIND USER BY ID ===*/
+    /*=== FIND USER BY EMAIL ===*/
     static findUserByEmail(email) {
         return new Promise((resolve, reject) => {
             User.findOne({ email: email })
@@ -31,8 +31,29 @@ class UserRepository {
         });
     }
 
+    /*=== FIND USER BY ID ===*/
+    static async findUserById (userId, next) {
+        try {
+            return await User.findById(userId, { _id: 1, email: 1, nickname: 1, roles: 1, registeredAt: 1, updatedAt: 1 });
+        }
+        catch (err) {
+            next(new InternalServerError());
+        }
+    }
+
+    /*=== USER EXISTS ===*/
+    static async userExists(userId, next) {
+        try {
+            const existingUser = await User.findById(userId);
+            return !!existingUser;
+        }
+        catch (err) {
+            next(new InternalServerError());
+        }
+    }
+
     /*=== GET ALL USERS ===*/
-    static async getAllUsers() {
+    static async getAllUsers(next) {
         try {
             return await User.find({}, 'id email nickname roles registeredAt updatedAt');
         }
@@ -40,16 +61,6 @@ class UserRepository {
             next(new InternalServerError());
         }
     }
-
-    /*=== GET SINGLE USER ===*/
-//     static async getUser() {
-//         try {
-//             return await User.find({}, 'id email nickname roles registeredAt updatedAt');
-//         }
-//         catch (err) {
-//             next(new InternalServerError());
-//         }
-//     }
 }
 
 
