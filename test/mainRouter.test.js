62f59d61900f5aa1616ad838bedde35b4d5ed910
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-12-29 19:15:42 +0100

    maj

--- test/mainRouter.test.js
+++ test/mainRouter.test.js
@@ -16,6 +16,7 @@ describe('MAIN ROUTER', () => {
     })
 
     /*=== /api ===*/
+    /*==== /api GET ====*/
     describe('TRY GET', () => {
 
         it('Should return 200', async () => {
@@ -34,6 +35,7 @@ describe('MAIN ROUTER', () => {
         });
     })
 
+    /*==== /api OPTIONS ====*/
     describe('TRY OPTIONS', () => {
 
         it('Should return 405', async () => {
@@ -42,10 +44,76 @@ describe('MAIN ROUTER', () => {
         });
     })
 
+    /*==== /api HEAD ====*/
     describe('TRY HEAD', () => {
 
         it('Should return 405', async () => {
             const response = await request(app).head('/api');
+            expect(response.status).toBe(405);
+        });
+    })
+
+
+    /*=== /api/login ===*/
+    /*==== /api/login POST ====*/
+    describe('TRY POST', () => {
+
+        it('Should return 404', async () => {
+            const response = await request(app).post('/api/login/ggrfd');
+            expect(response.status).toBe(404);
+        });
+    })
+
+    /*==== /api/login GET ====*/
+    describe('TRY GET', () => {
+
+        it('Should return 405', async () => {
+            const response = await request(app).get('/api/login');
+            expect(response.status).toBe(405);
+        });
+    })
+
+    /*==== /api/login PUT ====*/
+    describe('TRY PUT', () => {
+
+        it('Should return 405', async () => {
+            const response = await request(app).put('/api/login');
+            expect(response.status).toBe(405);
+        });
+    })
+
+    /*==== /api/login PATCH ====*/
+    describe('TRY PATCH', () => {
+
+        it('Should return 405', async () => {
+            const response = await request(app).patch('/api/login');
+            expect(response.status).toBe(405);
+        });
+    })
+
+    /*==== /api/login DELETE ====*/
+    describe('TRY DELETE', () => {
+
+        it('Should return 405', async () => {
+            const response = await request(app).delete('/api/login');
+            expect(response.status).toBe(405);
+        });
+    })
+
+    /*==== /api/login OPTIONS ====*/
+    describe('TRY OPTIONS', () => {
+
+        it('Should return 405', async () => {
+            const response = await request(app).options('/api/login');
+            expect(response.status).toBe(405);
+        });
+    })
+
+    /*==== /api/login HEAD ====*/
+    describe('TRY HEAD', () => {
+
+        it('Should return 405', async () => {
+            const response = await request(app).head('/api/login');
             expect(response.status).toBe(405);
         });
     })
