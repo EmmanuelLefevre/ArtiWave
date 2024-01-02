ec69544d081e3263031550bab530e713b0c4bc83
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2024-01-02 16:44:54 +0100

    maj

--- db.config.js
+++ db.config.js
@@ -34,20 +34,3 @@ const closeDB = async () => {
 
 /*============ EXPORT MODULE ============*/
 module.exports = {connectDB, closeDB};
-
-
-
-
-// /*============ DATABASE SINGLETON CONNEXION WITH PROMISE ============*/
-// const connectDB = () => {
-//     const mongoURI = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-
-//     return mongoose.connect(mongoURI)
-//         .then(() => {
-//             console.log('MongoDB is connected!');
-//         })
-//         .catch((err) => {
-//             console.error('Database connection error!', err);
-//             process.exit(1);
-//         });
-// };
