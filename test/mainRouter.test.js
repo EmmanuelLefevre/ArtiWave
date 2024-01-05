58c4b255b0deebb09a56b117c46953e28792333a
Emmanuel Lefevre <waiasiaa@protonmail.com>
2024-01-05 16:31:05 +0100

    maj

--- test/mainRouter.test.js
+++ test/mainRouter.test.js
@@ -6,7 +6,7 @@
 /*============ IMPORT USED MODULES ============*/
 const request = require('supertest');
 const app = require('../app');
-const { closeDB, connectDB } = require('../api/db.config');
+const { closeDB } = require('../api/db.config');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 
@@ -26,10 +26,6 @@ jest.mock('express-rate-limit', () => {
 
 /*============ MAIN ROUTER TESTS ============*/
 describe('MAIN ROUTER', () => {
-    beforeEach(async () => {
-        await connectDB();
-    });
-
     afterAll(async () => {
         await closeDB();
     })
