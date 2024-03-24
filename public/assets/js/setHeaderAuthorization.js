0979b990fb54fa51740e6145c38707a1894007af
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2024-03-24 14:39:42 +0100

    new

--- public/assets/js/setHeaderAuthorization.js
+++ public/assets/js/setHeaderAuthorization.js
@@ -17,3 +17,30 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     // Attacher l'événement de clic à un élément parent existant
+//     document.addEventListener('click', function(event) {
+//         // Vérifier si l'élément cliqué est le bouton "articles-list"
+//         if (event.target && event.target.id === 'articles-list') {
+//             // Add access token to request as header
+//             htmx.on("htmx:configRequest", function(event) {
+//                 // Get token
+//                 const token = getAccessToken();
+
+//                 // Check if token exists
+//                 if (token) {
+//                     // Build Authorization value
+//                     let authorizationValue = `Bearer ${token}`;
+
+//                     // Set authorization header in request
+//                     event.detail.headers["Authorization"] = authorizationValue;
+//                 }
+//                 else {
+//                     console.error("Access token is missing!");
+//                 }
+//             });
+//         }
+//     });
+// });
+
