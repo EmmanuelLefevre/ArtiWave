7d8c3479fa3dc04186bb027625b5702d60535085
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2024-03-24 17:08:06 +0100

    add function to create login button

--- public/assets/js/buttons.js
+++ public/assets/js/buttons.js
@@ -1,3 +1,21 @@
+function createLoginButton() {
+  const loginButton = document.createElement('button');
+	loginButton.setAttribute('hx-get', '/login-component');
+	loginButton.setAttribute('hx-swap', 'outerHTML');
+	loginButton.setAttribute('id', 'login-button');
+	loginButton.setAttribute('type', 'submit');
+	loginButton.setAttribute('aria-label', 'Bouton vers le formulaire de connexion');
+
+	// Create the span element for the label
+	const spanLabel = document.createElement('span');
+	spanLabel.textContent = 'Connexion';
+
+	// Append the span to the button
+	loginButton.appendChild(spanLabel);
+
+  return loginButton;
+}
+
 function createLogoutButton() {
   // Create the button element
   const logoutButton = document.createElement('button');
