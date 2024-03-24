e0f3facff3d45c834234850fbfd44f9d51fe5c77
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2024-03-24 16:41:21 +0100

    add logout logic

--- public/assets/js/logout.js
+++ public/assets/js/logout.js
@@ -1,28 +1,36 @@
-// Select disconnection button
-const logoutButton = document.getElementById('logout-button');
-
-document.getElementById('logout-button').addEventListener('clic', function() {
-  // Delete token from local storage
-  deleteAccessToken();
-
-  // Redirect to homepage
-  window.location.href = '/';
-
-  // Create the button element
-  const loginButton = document.createElement('button');
-  loginButton.setAttribute('hx-get', '/login-component');
-  loginButton.setAttribute('hx-swap', 'outerHTML');
-  loginButton.setAttribute('id', 'login-button');
-  loginButton.setAttribute('type', 'submit');
-  loginButton.setAttribute('aria-label', 'Bouton vers le formulaire de connexion');
-
-  // Create the span element for the label
-  const spanLabel = document.createElement('span');
-  spanLabel.textContent = 'Connexion';
-
-  // Append the span to the button
-  loginButton.appendChild(spanLabel);
-
-  // Replace disconnection button by login one
-  logoutButton.parentNode.replaceChild(loginButton, logoutButton);
+document.addEventListener('click', function(event) {
+	// Check if the clicked item is the logout button
+	if (event.target && event.target.closest('#logout-button')) {
+		handleDisconnection();
+	}
 });
+
+function handleDisconnection() {
+	// Delete token from local storage
+	deleteAccessToken();
+
+	// Redirect to homepage
+	window.location.href = '/';
+
+	// Create the button element
+	const loginButton = document.createElement('button');
+	loginButton.setAttribute('hx-get', '/login-component');
+	loginButton.setAttribute('hx-swap', 'outerHTML');
+	loginButton.setAttribute('id', 'login-button');
+	loginButton.setAttribute('type', 'submit');
+	loginButton.setAttribute('aria-label', 'Bouton vers le formulaire de connexion');
+
+	// Create the span element for the label
+	const spanLabel = document.createElement('span');
+	spanLabel.textContent = 'Connexion';
+
+	// Append the span to the button
+	loginButton.appendChild(spanLabel);
+
+	// Select logout button
+	const logoutButton = document.getElementById('logout-button');
+
+	// Replace disconnection button by login one
+	loginButton.parentNode.replaceChild(loginButton, logoutButton);
+}
+
