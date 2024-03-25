4bfbf65055676cd070273a031ab1a7a9e7b30052
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2024-03-25 17:49:44 +0100

    var => const

--- public/assets/js/activeNavLink.js
+++ public/assets/js/activeNavLink.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var currentPath = window.location.pathname;
-    var homePath = "/";
-    var articlesPath = "/articles";
+    const currentPath = window.location.pathname;
+    const homePath = "/";
+    const articlesPath = "/articles";
 
     // Function to add active class to the corresponding link
     function setActiveLink(linkId) {
         // Remove active class from all links
-        var links = document.querySelectorAll('.nav-link');
+        const links = document.querySelectorAll('.nav-link');
         links.forEach(function(link) {
             link.classList.remove('active');
         });
         // Add active class to the specified link
-        var activeLink = document.getElementById(linkId);
+        const activeLink = document.getElementById(linkId);
         if (activeLink) {
             activeLink.classList.add('active');
         }
