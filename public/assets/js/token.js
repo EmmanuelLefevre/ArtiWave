80a01ab9b1907040a82d7ed94a7ba66cf1373a1c
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2024-03-26 19:46:19 +0100

    add function to parse jwt and get nickname

--- public/assets/js/token.js
+++ public/assets/js/token.js
@@ -1,20 +1,58 @@
 // Get access token from local storage
 function getAccessToken() {
-  return localStorage.getItem('artiwave_access_token');
+  // Local storage not available in execution environment
+  if (typeof localStorage !== 'undefined') {
+    return localStorage.getItem('artiwave_access_token');
+  }
+  return null;
 }
 
 // Set access token to local storage
 function setAccessToken(token) {
-  localStorage.setItem('artiwave_access_token', token);
+  // Local storage not available in execution environment
+  if (typeof localStorage !== 'undefined') {
+    if (token !== undefined) {
+      localStorage.setItem('artiwave_access_token', token);
+    }
+    else {
+      deleteAccessToken();
+    }
+  }
 }
 
 // Delete access token from local storage
 function deleteAccessToken() {
-  localStorage.removeItem('artiwave_access_token');
+  // Local storage not available in execution environment
+  if (typeof localStorage !== 'undefined') {
+    const accessToken = localStorage.getItem('artiwave_access_token');
+    if (accessToken !== null) {
+      localStorage.removeItem('artiwave_access_token');
+    }
+  }
 }
 
 // Get connection status
 function getConnectionStatus() {
   // Value converted to boolean
   return !!getAccessToken();
+}
+
+// Parse token
+function parseToken(token) {
+  if (!token) {
+    return;
+  }
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace("-", "+").replace("_", "/");
+  return JSON.parse(window.atob(base64));
+}
+
+// Get nickname from token
+function getNicknameFromToken(tokenContent) {
+  // Checks if token content is defined nickname key is present in token
+  if (!tokenContent || !tokenContent.nickname) {
+    return null;
+  }
+  // Returns value of nickname key
+  return tokenContent.nickname;
 }
