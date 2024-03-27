0ac129b38fd6dfccd37cc3faad81975c1538e0c0
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2024-03-27 21:08:13 +0100

    maj

---
+++ public/assets/js/buttons.js
@@ -0,0 +1,50 @@
+/* ========== BUTTON ==========*/
+
+class Button {
+  constructor(text, id, ariaLabel) {
+    this.button = document.createElement('button');
+    this.button.setAttribute('id', id);
+    this.button.setAttribute('type', 'submit');
+    this.button.setAttribute('aria-label', ariaLabel);
+
+    // Create span text
+    const spanLabel = document.createElement('span');
+    spanLabel.textContent = text;
+
+    // Add span to button
+    this.button.appendChild(spanLabel);
+  }
+
+  // Create button method
+  createButton(parentElement) {
+    parentElement.appendChild(this.button);
+  }
+
+  // Replace button method
+  replaceButton(newButton) {
+    const parentElement = this.button.parentNode;
+    parentElement.replaceChild(newButton.button, this.button);
+  }
+
+}
+
+
+class LoginButton extends Button {
+  constructor() {
+    super('Connexion', 'login-button', 'Bouton vers le formulaire de connexion');
+    this.button.setAttribute('hx-get', '/login-component');
+    this.button.setAttribute('hx-swap', 'outerHTML');
+  }
+}
+
+
+class LogoutButton extends Button {
+  constructor() {
+    super('Déconnexion', 'logout-button', 'Bouton de déconnexion');
+  }
+
+  // Render logout button in DOM
+  render() {
+    return this.button;
+  }
+}
