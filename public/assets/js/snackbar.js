f9ef55f8458bfa02a9ea69c683a63593b467da6e
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2024-03-28 09:30:29 +0100

    maj

--- public/assets/js/snackbar.js
+++ public/assets/js/snackbar.js
@@ -1,10 +1,11 @@
 /* ========== SNACKBAR ==========*/
 
 class Snackbar {
-  constructor(message, id, ariaLabel, animationClass = null, duration = null) {
+  constructor(message, id, snackbarClass, ariaLabel, animationClass = null, duration = null) {
     this.snackbar = document.createElement('div');
     this.snackbar.textContent = message;
     this.snackbar.setAttribute('id', id);
+    this.snackbar.classList.add(snackbarClass);
     this.snackbar.setAttribute('aria-label', ariaLabel);
     if (duration) {
       this.duration = duration;
@@ -27,27 +28,27 @@ class Snackbar {
 
 class SnackbarInfo extends Snackbar {
   constructor(message) {
-    super(message, 'snackbar-info', 'Notification d\'information', 'slide-up', '3000');
+    super(message, 'snackbar', 'snackbar-info', 'Notification d\'information', 'slide-up', '3000');
   }
 }
 
 
 class SnackbarSuccess extends Snackbar {
   constructor(message) {
-    super(message, 'snackbar-success', 'Notification de validation');
+    super(message, 'snackbar', 'snackbar-success', 'Notification de validation');
   }
 }
 
 
 class SnackbarWarning extends Snackbar {
   constructor(message) {
-    super(message, 'snackbar-warning', 'Notification d\'avertissement');
+    super(message, 'snackbar', 'snackbar-warning', 'Notification d\'avertissement');
   }
 }
 
 
 class SnackbarAlert extends Snackbar {
   constructor(message) {
-    super(message, 'snackbar-alert', 'Notification d\'alerte');
+    super(message, 'snackbar', 'snackbar-alert', 'Notification d\'alerte');
   }
 }
