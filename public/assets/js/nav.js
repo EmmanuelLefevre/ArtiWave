e2296d6dc90e745b5d32d37f952ffac31abfe39d
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2024-04-01 17:41:44 +0200

    rename

---
+++ public/assets/js/nav.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+	const currentPath = window.location.pathname;
+	const homePath = "/";
+	const articlesPath = "/articles";
+
+	/* Active Link */
+	// Function to add active class to the corresponding link
+	function setActiveLink(linkId) {
+		// Remove active class from all links
+		const links = document.querySelectorAll('.nav-link');
+		links.forEach(function(link) {
+			link.classList.remove('active');
+		});
+		// Add active class to the specified link
+		const activeLink = document.getElementById(linkId);
+		if (activeLink) {
+			activeLink.classList.add('active');
+		}
+	}
+
+	// Check current path and call the setActiveLink function consequently
+	if (currentPath === homePath) {
+		setActiveLink('home-link');
+	} else if (currentPath === articlesPath) {
+		setActiveLink('articles-list-link');
+	}
+
+	/* Disabled Nav Link */
+	function disabledNavLink() {
+		var disabledLink = document.getElementById('articles-list-link');
+		var userConnected = getConnectionStatus();
+
+		if (!userConnected) {
+			disabledLink.style.display = 'none';
+		}
+	}
+
+	window.onload = disabledNavLink;
+
+});
