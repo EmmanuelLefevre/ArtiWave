68dcde9e0b3ba763db1886b586d882b3ab1a0551
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-11-08 18:02:58 +0100

    fix button swap

--- public/assets/js/buttons.js
+++ public/assets/js/buttons.js
@@ -1,10 +1,11 @@
 /* ========== BUTTON ==========*/
 
 class Button {
-  constructor(label, id, ariaLabel) {
+  constructor(label, id, ariaLabel, buttonType) {
     this.button = document.createElement('button');
     this.button.setAttribute('id', id);
     this.button.setAttribute('aria-label', ariaLabel);
+    this.button.setAttribute('type', buttonType);
 
     // Create label text
     const spanLabel = document.createElement('span');
@@ -22,39 +23,32 @@ class Button {
   // Replace button
   replaceButton(newButton) {
     const parentElement = this.button.parentNode;
-    parentElement.replaceChild(newButton.button, this.button);
+    if (parentElement) {
+      parentElement.replaceChild(newButton.button, this.button);
+    }
   }
-
 }
 
 
 class LoginButton extends Button {
   constructor() {
-    super('Connexion', 'login-button', 'Bouton vers le formulaire de connexion');
+    super('Connexion', 'login-button', 'Bouton vers le formulaire de connexion', 'submit');
     this.button.setAttribute('href', '#login');
     this.button.setAttribute('hx-get', '/login-component');
     this.button.setAttribute('hx-swap', 'outerHTML');
-    this.button.setAttribute('type', 'submit');
   }
 }
 
 
 class LogoutButton extends Button {
   constructor() {
-    super('Déconnexion', 'logout-button', 'Bouton de déconnexion');
-    this.button.setAttribute('type', 'button');
-  }
-
-  // Render logout button in DOM
-  render() {
-    return this.button;
+    super('Déconnexion', 'logout-button', 'Bouton de déconnexion', 'button');
   }
 }
 
 
 class SubmitLoginFormButton extends Button {
   constructor() {
-    super('Se connecter', 'submit-login-form-button', 'Bouton pour soumettre le formulaire de connexion');
-    this.button.setAttribute('type', 'submit');
+    super('Se connecter', 'submit-login-form-button', 'Bouton pour soumettre le formulaire de connexion', 'submit');
   }
 }
