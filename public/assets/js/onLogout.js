b526881db15589bdac7c7ae7de4a7a418f827a53
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-11-08 18:02:58 +0100

    fix button swap

--- public/assets/js/onLogout.js
+++ public/assets/js/onLogout.js
@@ -1,32 +1,45 @@
 import { navigateToHome } from './router.js';
 
 document.addEventListener('click', function(event) {
-	// Check if the clicked item is the logout button
-	const logoutBtn = event.target.closest('#logout-button');
-	if (!logoutBtn) return;
+	const logoutBtnEl = event.target.closest('#logout-button');
+	if (!logoutBtnEl) return;
 
-	handleDisconnection();
+	const logoutBtn = new LogoutButton();
+  logoutBtn.button = logoutBtnEl;
+
+	handleDisconnection(logoutBtn);
 });
 
-function handleDisconnection() {
+function handleDisconnection(logoutBtn) {
 	// Get token
 	const token = getAccessToken();
 
 	// Decode token
-  const decodedToken = parseToken(token);
+	const decodedToken = parseToken(token);
 
-	// Get nickname from decoded token
+	// Get nickname
 	const nickname = getNicknameFromToken(decodedToken);
 
 	// Snackbar
 	const infoSnackbar = new SnackbarInfo(`Au revoir ${nickname}.`);
-  infoSnackbar.createSnackbar();
+	infoSnackbar.createSnackbar();
 
 	// Delete token from local storage
 	deleteAccessToken();
 
-	// Redirect to homepage
-	setTimeout(() => {
-    navigateToHome();
-  }, 500);
+	// New connexion button
+	const loginBtn = new LoginButton();
+	logoutBtn.replaceButton(loginBtn);
+
+	// Force HTMX to process the new button
+	if (window.htmx) {
+		htmx.process(loginBtn.button);
+	}
+
+	// Redirect to homepage if not on /home
+	if (window.location.pathname !== '/home') {
+		setTimeout(() => {
+			navigateToHome();
+		}, 500);
+	}
 }
